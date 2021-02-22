54a3f805025c2b63bc0460a7f99fa057b9b2effb
Angelina Ebreo <angelinaebreo@gmail.com>
2021-02-21 21:08:29 -0500

    fixed broken DOM

--- index.js
+++ index.js
@@ -1,87 +1,130 @@
 
-const disemvowel = () => {
-    const el = document.querySelector("name-to-disemvowel");
-    const p = document.getElementById("#disemvowel-string");
+const disEmvowel = () => {
+    const el = document.querySelector("#name_to_disemvowel");
+    const p = document.getElementById("disemvowel-string");
     const str = el.value;
-    const output = "";
-    const vowels = "aeiou"; 
-    for(const char in str) {
-        if(vowels.includes(char.toUpperCase())) {
-            output += char;
+    let output = "";
+    const vowels = "aeiouAEIOU"; 
+    for(let i = 0; i < str.length; i++) {
+        if(!vowels.includes(str[i])) {
+            output += str[i];
+        } else {
+            output += ""
         }
     }
-    p.textContents = output;
+    p.textContent = output;
+   console.log(output)
 }
 
 const isPalindrome = () => {
-    const str = document.querySelector("#is-palindrome-input");
-    const p = document.querySelectorAll("#is-palindrome-p");
+    const str = document.querySelector("#is-palindrome-input").value;
+    const p = document.querySelector("#is-palindrome-p");
     let result = "true"; 
-    for(let i = 0; i < str; i++) {
-        if(str[i] === str[str.length - i - 1]) {
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] !== str[str.length - i - 1]) {
             result = "false"
         }
     }
-    result = p.textContent;
+  
+    p.textContent = result;
 }
-const sum = (arr) => {
-    let sum = 0; 
-    for(let i = 0; i <= arr.length; i++) {
-        const num = arr[i];
-        sum += num
-    }
-    return sum; 
-}
-const average = (arr) => {
-    arr = [1, 2, 3];
-    let sumOfNums = sums(arr) 
-    return sumOfNums / arr.length; 
-}
+// const sum = (arr) => {
+//     let sum = 0; 
+//     for(let i = 0; i <= arr.length; i++) {
+//         const num = arr[i];
+//         sum += num
+//     }
+//     return sum; 
+// }
+// const average = (arr) => {
+//     // arr = [1, 2, 3];
+//     let sumOfNums = sums(arr) 
+//     return sumOfNums / arr.length; 
+// }
 
-const onlyOdds = arr => {
-     arr.filter(num => num % 3 === 1)
-}
+// const onlyOdds = arr => {
+//      arr.filter(num => num % 3 === 1)
+// }
 
 const favoriteNumbers = () => {
-    let list = Number(document.querySelector("#favorite-numbers < li").value);
+    let list = document.querySelectorAll("ul>li")
+
+
     let arr = [];
     list.forEach((listItem) => {
-        arr.push(listItem.textContent)
+        arr.push(Number(listItem.textContent))
     })
-    const sum = sum(arr);
+
+
+  
+    const sum = (arr) => {
+        let sum = 0; 
+        for(let i = 0; i < arr.length; i++) {
+            const num = arr[i];
+            sum += num
+        }
+        return sum; 
+    }
+    
+    const sumOfNums = sum(arr);
+    const length = arr.length;
+   
+
+    const average = (arr) => {
+        return sumOfNums / length; 
+    }
+    
     const averageOfNums = average();
+ 
+
+   
+    const onlyOdds = arr => {
+      return arr.filter(num => num % 2 === 1)
+    
+        
+   }
+
     const onlyOddsOfNums = onlyOdds(arr);
 
-    const sumOfFav = Number(document.querySelector("#sum-of-favorite-nums"));
-    sumOfFav.textContent += sumOfNums; 
+
+
+    const sumOfFav = document.querySelector("#sum-of-favorite-nums");
+    sumOfFav.textContent += ` ${sumOfNums}`; 
 
     const aveOfFav = document.querySelector("#average-of-favorite-nums");
-    aveOfFav.textContent += averageOfNums; 
-
-    var oddList = document.querySelector("#is-palindrome-p");
+    aveOfFav.textContent += ` ${averageOfNums}`; 
     
-    const li = document.createElement("h1");
-    onlyOddsOfNums.each((odd) => {
-      li.inerText = odd;
-      oddList.removeChild(li);
+
+    const oddList = document.querySelector("#list-of-odd-favorite-nums");
+    
+    
+    onlyOddsOfNums.forEach((odd) => {
+        const li = document.createElement("li");
+      li.textContent = odd;
+      oddList.appendChild(li);
     });
-}
+ }
 
 favoriteNumbers();
 
 const incrementCount = () => {
-    const countr = Number(document.querySelector("#click-count"));
-    counter.textContent = counter.textContent + 1;
+    let clickCount = document.querySelector("#click-count")
+    let counter = Number(clickCount.textContent)
+
+    counter += 1;
+    clickCount.textContent = counter
+
+
 }
 
 const reset = () => {
-    const counter = document.querySelector("#click-count");
-    counter.displayContent = 0;
+    const clickCount = document.querySelector("#click-count");
+    clickCount.textContent = 0;
 }
 
 const addItem = () => {
-    const shoppingList = document.querySelector("ul");
-    const input = document.querySelector(".add-item")
+    const shoppingList = document.querySelector(".shopping-list");
+    const input = document.querySelector("#add-item").value
     const item = input;
     const listItem = document.createElement("li");
     listItem.textContent = item;
@@ -91,7 +134,7 @@ const addItem = () => {
 
 const killButton = () => {
     const button = document.querySelector("#kill-button");
-    button.removeChild(button)
+    button.parentNode.removeChild(button)
 
     const header = document.querySelector("#kill-button-header");
     header.innerText = "Yay you were victorious!"
